474b89d368183cc38fbc2396eb67528b4f7bbf46
Elena Blgr <elenablgr@MacBook-Pro-Elena.local>
2023-09-28 19:35:26 +0300

    transformed from big to decimal

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "s21_decimal.h"
+
+int main(){
+    s21_big_decimal dec = {5,0,0,0,0,0,0,0};
+    s21_big_decimal dec2 = {5,0,0,0,0,0,0,0};
+    s21_big_decimal res = {0,0,0,0,0,0,0,0};
+    
+    printf("\n***** DECIMAL 1 *****\n\n");
+    s21_print_decimal(&dec);
+
+    printf("\n***** DECIMAL 2 *****\n\n");
+    s21_print_decimal(&dec2);
+
+    printf("\n***** RESULT *****\n\n"); 
+    s21_print_decimal(&res); 
+
+    return 0;
+}
