59bc047cf237aee4aaa60b4155fb1c46cdc0352d
Elena Blgr <elenablgr@MacBook-Pro-Elena.local>
2023-09-30 03:33:54 +0300

    main jff

--- src/main.c
+++ src/main.c
@@ -5,14 +5,7 @@ int main(){
     s21_decimal dec1 = {{56, 0, 0, 0xffffffff}}; //0b00000000000000010000000000000000
     s21_decimal dec2 = {{21, 0, 0, 0b00000000000000110000000000000000}};
     s21_decimal res = {{0, 0, 0, 0}};    
-
-    //56 /10^1+ 21 а он вместо 26,6 /10^1 выдает 76
-    // gcc -Werror -Wextra -Wall *.c
-    //0b11111111;
-    //0b00000011;
-    0b111000;
-    //printf("%d\n", s21_get_pow(&dec));
-
+    
     printf("\n***** DECIMAL 1 *****\n\n");
     s21_print_decimal(&dec1);
 
