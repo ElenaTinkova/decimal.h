3b366ec699efa66124ee0fc4c9ac69641b54752e
Markov Vladislav <markov.v.d@mail.ru>
2023-10-01 15:20:09 +0300

    Add funs

---
+++ src/s21_add.c
@@ -0,0 +1,19 @@
+#include "s21_decimal.h"
+#include <stdio.h>
+
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  int error = 0; //Код ошибки
+  s21_big_decimal value1 = s21_enlarge_D(value_1);
+  s21_big_decimal value2 = s21_enlarge_D(value_2);
+  s21_big_decimal result1 = s21_enlarge_D(*result);
+  
+  s21_add_big_decimal(value1, value2, &result1);
+  //Проверка на переполнение мантиссы
+  int check = s21_overflow(&result1);
+  if(!check){
+    *result = s21_cut_D(result1);
+  }
+  error = check; //Присвоение вывод overflow к error переменной
+
+  return error;
+}
---
+++ src/s21_from_int_to_decimal.c
@@ -0,0 +1,15 @@
+#include     "s21_decimal.h"
+//Из int в decimal
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst){
+    if((src > -2147483647) && (src < 2147483647)){
+        if (src < 0){
+            src = src * -1;
+            s21_set_sign(dst, 1);
+        }
+        dst->bits[0] = src;
+        return 0; //OK
+    }else{
+        return 1; //1 - ошибка конвертации
+    }
+}
