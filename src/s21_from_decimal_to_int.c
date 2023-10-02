be64f15f172bac3a5fb583c82c0cd397fcffb3e6
leyapatty <leyapatty@leyapattys-MacBook-Pro.local>
2023-10-02 22:16:00 +0300

    tests

--- src/s21_from_decimal_to_int.c
+++ src/s21_from_decimal_to_int.c
@@ -8,19 +8,18 @@ int s21_from_decimal_to_int(s21_decimal src, int *dst) {
     int sign = s21_get_bit(&src, 127);
     int sign1 = s21_get_bit(&src, 31);
     int scale = s21_get_pow(&src);
-    double rez = src.bits[0] / pow(10, scale);
-    if (src.bits[0] <= 2147483647){
-      *dst = src.bits[0];
-      if (sign || sign1){
-        *dst *= -1;
-      }   
-    } else error = 1;
+    if((scale) > 0){
+        s21_truncate(src, &src);
+    }
+    if (src.bits[0] > 2147483647){
+        error = 1; 
+    } else *dst = src.bits[0]; {
+        if (sign || sign1){
+          *dst *= -1;
+        }  
+      }
     if (src.bits[1] || src.bits[2]){
         error = 1;
     }
-    if((scale) > 0){
-        *dst = trunc(rez);
-    }
     return error;
-    return 0;
 }
