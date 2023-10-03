4b0e6c30030b1699a0d8c8fcc4aa50fa8514c788
root <root@Admin>
2023-10-03 20:12:14 +0300

    Add from_float_to_decimal by Makar

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -59,7 +59,7 @@ int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2);         //+
 
 //Преобразователи
 int s21_from_int_to_decimal(int src, s21_decimal *dst);  //+
-// int s21_from_float_to_decimal(float src, s21_decimal *dst);
+int s21_from_float_to_decimal(float src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);      //+
 int s21_from_decimal_to_float(s21_decimal src, float *dst);  //+
 
